7d0eb5f966111404b9d4ee682c6ac6254cf1432d
abauruel <abauruel@gmail.com>
2021-04-07 20:58:47 -0300

    feat: add service, environment and secret

--- server.go
+++ server.go
@@ -1,12 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
+
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello %s, I have %s years old", name, age)
+}
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %s", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
+}
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv(("USER"))
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "My USER: %s. PASSWORD: %s", user, password)
 }
